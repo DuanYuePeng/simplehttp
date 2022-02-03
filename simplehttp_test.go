9010abd91dfb57d5eadf670c13b0a842780695bb
Elliot Duan <ayusuki@163.com>
2022-02-03 18:09:21 +0800

    add test

---
+++ simplehttp_test.go
@@ -0,0 +1,48 @@
+package simplehttp
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type SomeResponse struct {
+	Message string
+}
+
+func TestDoGraphQL(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		reqBytes, _ := io.ReadAll(r.Body)
+		reqStr := string(reqBytes)
+
+		if reqStr != `{"query":"query($arg:String!){hero{name}}","variables":{"arg":"someArgument"}}` {
+			t.Fatalf("bad request body: %s", reqStr)
+		}
+
+		reqAuthHeader := r.Header.Get("Authorization")
+		if reqAuthHeader != "Bearer some-token" {
+			t.Fatalf("bad request header: %s", reqAuthHeader)
+		}
+
+		fmt.Fprintln(w, `{"message":"Hello"}`)
+	}))
+	defer server.Close()
+
+	respBody := new(SomeResponse)
+	err := DoGraphQL(server.URL, "query($arg:String!){hero{name}}", map[string]interface{}{
+		"arg": "someArgument",
+	}, map[string]string{
+		"Authorization": "Bearer some-token",
+	}, respBody)
+
+	if err != nil {
+		t.Fatalf("got error: %s", err.Error())
+	}
+
+	if respBody.Message != "Hello" {
+		t.Fatalf("bad response body: %s", respBody.Message)
+	}
+}
